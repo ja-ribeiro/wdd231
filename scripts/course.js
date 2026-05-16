b7de6d2a5ed9d028d5fb351fdbfbd74456e73e3c
ja-ribeiro <jribeiro1@byupatwhay.edu>
2026-05-16 18:03:03 -0300

    foi corrigo layout, cor e adicionado fotos

--- scripts/course.js
+++ scripts/course.js
@@ -1,8 +1,7 @@
 const courses = [
   { code: "WDD130", credits: 3, completed: true, type: "WDD" },
   { code: "WDD131", credits: 3, completed: true, type: "WDD" },
-  { code: "WDD231", credits: 3, completed: false, type: "WDD" },
-  { code: "CSE111", credits: 3, completed: false, type: "CSE" }
+  { code: "WDD231", credits: 3, completed: false, type: "WDD" }
 ];
 
 const container = document.getElementById("courseContainer");
@@ -13,18 +12,22 @@ function displayCourses(list) {
 
   list.forEach(course => {
     const div = document.createElement("div");
-    div.textContent = course.code;
     div.classList.add("course");
 
     if (course.completed) {
       div.classList.add("completed");
     }
 
+    div.innerHTML = `
+      <strong>${course.code}</strong><br>
+      ${course.credits} credits
+    `;
+
     container.appendChild(div);
   });
 
   const total = list.reduce((sum, c) => sum + c.credits, 0);
-  totalCredits.textContent = `Total credits: ${total}`;
+  totalCredits.textContent = `The total credits for courses listed above is ${total}`;
 }
 
 document.getElementById("all").addEventListener("click", () => {
