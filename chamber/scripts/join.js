16d49f4f185bbe41b57bbfffe4d0f61bf9f561d5
ja-ribeiro <jribeiro1@byupatwhay.edu>
2026-06-05 14:12:51 -0300

    alteracoes feitas no html para mostrar valor e no css foi arrumado o jeito que estava exposto as coisas

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // timestamp
   document.getElementById("timestamp").value = new Date().toISOString();
 
-  // abrir modal
   document.querySelectorAll("[data-modal]").forEach(btn => {
     btn.addEventListener("click", () => {
       document.getElementById(btn.dataset.modal).showModal();
     });
   });
 
-  // fechar modal
   document.querySelectorAll(".close").forEach(btn => {
     btn.addEventListener("click", () => {
       btn.closest("dialog").close();
