14c056d6c8c4067b0d6ee26eafc5e2c20af6d8aa
ja-ribeiro <jribeiro1@byupatwhay.edu>
2026-06-06 17:00:04 -0300

    corrigido css e foi adicionado o h1, corrigido url da og:image

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  // timestamp
   document.getElementById("timestamp").value = new Date().toISOString();
 
+  // last modified
+  document.getElementById("lastModified").textContent = document.lastModified;
+
+  // modals
   document.querySelectorAll("[data-modal]").forEach(btn => {
     btn.addEventListener("click", () => {
       document.getElementById(btn.dataset.modal).showModal();
